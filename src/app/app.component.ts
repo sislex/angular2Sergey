8aef4473e54a0d23638cfcbd80a24fbb0d114ec1
Sergei <3Hopkins@mail.ru>
2017-04-06 22:43:35 +0300

    hard select

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,15 @@ private  title2: any = null;
 private arr1 = null;
 private arr2 = null;
 
+  private arr_test: string[] = [
+    'Пункт1',
+    'Пункт2',
+    'Пункт3',
+    'Пункт4',
+    'Пункт5',
+  ];
+  private selectedArr: string[] = [];
+
 private arr = [
   {
     'name': 'name1',
@@ -31,9 +40,66 @@ private arr = [
   },
 ];
 
+ private nameListGroupMuscles = [
+    {
+      'name_group_muscles' : 'Грудные',
+      'nameListExercises' : [
+        'Жим гантелей на горизонтальной скамье',
+        'Сведение рук в кроссовере на верхнюю часть груди'
+      ]
+    },
+    {
+      'name_group_muscles' : 'Бицепс',
+      'nameListExercises' : [
+        'Молотки',
+        'Двойное сгибание рук стоя',
+        'Сгибание рук стоя в наклоне по очереди'
+      ]
+    },
+    {
+      'name_group_muscles' : 'Трицепс',
+      'nameListExercises' : [
+        'Французский жым',
+        'Поднятие гантели из-за головы',
+        'Кикбэк'
+      ]
+    },
+  ]; 
+  
+
 constructor(private clickHistoryService: ClickHistoryService){
   console.log(clickHistoryService.namesList);
 }
 
+  rows(): undefined[]{
+    let arr: undefined[] = [];
+
+    if(this.selectedArr.length == 0){
+      arr = new Array(1);
+    } else if(this.selectedArr.length < this.arr_test.length){
+      arr = new Array(this.selectedArr.length + 1);
+    }else{
+      arr = new Array(this.selectedArr.length);
+    }
+
+    return arr;
+  }
+
+  possibleSelect(itemFromSelect: string = ''): string[]{
+    let arr_test: string[] = [];
+
+    for(let item of this.arr_test){
+      if(itemFromSelect != item){
+        if(this.selectedArr.indexOf(item) ==- 1){
+          arr_test.push(item);
+        }
+      } else {
+        arr_test.push(item);
+      }
+
+    }
+
+    return arr_test;
+  }
 
 }
